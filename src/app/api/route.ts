06de6bf8acde5057578dcda9d7d85e977e9d6974
TemaSaur <69428015+TemaSaur@users.noreply.github.com>
2024-04-05 00:01:13 +0500

    just fuck you

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,16 +1,4 @@
-import type { NextApiRequest, NextApiResponse } from "next"
 import { NextResponse } from "next/server"
-
-// type ResponseData = {
-// 	message: string
-// }
-
-// export default function handler(
-// 	req: NextApiRequest,
-//     res: NextApiResponse<ResponseData>
-// ) {
-// 	res.status(200).json({ message: 'Hello from Next.js!' })
-// }
 
 
 export async function GET() {
